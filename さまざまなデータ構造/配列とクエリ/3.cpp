41ec381c498db7776939d7c04065a0ede3cc5a75
jion-kozono <zeon66190329@gmail.com>
2022-08-13 13:35:34 +0900

    complete: 配列とクエリ

---
+++ さまざまなデータ構造/配列とクエリ/3.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rep3(i, m, n) for(int i = (m); i < (int)(n); i++)
+#define ALL(x) x.begin(), x.end()
+#define debug(var)                                                             \
+  do {                                                                         \
+    view(var);                                                                 \
+  } while(0)
+#define debugN(var)                                                            \
+  do {                                                                         \
+    std::cout << #var << " : ";                                                \
+    view(var);                                                                 \
+  } while(0)
+template <typename T> void view(T e) { std::cout << e << std::endl; }
+template <typename T> void view(const std::vector<T> &v) {
+  for(const auto &e : v) {
+    std::cout << e << " ";
+  }
+  std::cout << std::endl;
+}
+template <typename T> void view(const std::vector<std::vector<T>> &vv) {
+  for(const auto &v : vv) {
+    view(v);
+  }
+}
+
+int main() {
+  int array[10] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+  int q;
+  cin >> q;
+
+  rep(i, q) {
+    int writeOrOutput;
+    cin >> writeOrOutput;
+    bool isWrite = writeOrOutput == 1;
+    if(isWrite) {
+      int k, v;
+      cin >> k >> v;
+      array[k] = v;
+    } else {
+      int k;
+      cin >> k;
+      debug(array[k]);
+    }
+  }
+  return 0;
+}
