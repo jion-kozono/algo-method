2b0a2f72467f8a920b69a019a1cd1ffc35a47229
jion-kozono <zeon66190329@gmail.com>
2022-08-26 08:36:13 +0900

    second: 工夫\ \(1\)\ -\ for\ 文のループを減らす

---
+++ 計算量/工夫 (1) - for 文のループを減らす/second/3.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rep3(i, m, n) for(int i = (m); i < (int)(n); i++)
+#define ALL(x) x.begin(), x.end()
+#define debug(var)                                                             \
+  do {                                                                         \
+    view(var);                                                                 \
+  } while(0)
+#define debugN(var)                                                            \
+  do {                                                                         \
+    std::cout << #var << " : ";                                                \
+    view(var);                                                                 \
+  } while(0)
+template <typename T> void view(T e) { std::cout << e << std::endl; }
+template <typename T> void view(const std::vector<T> &v) {
+  for(const auto &e : v) {
+    std::cout << e << " ";
+  }
+  std::cout << std::endl;
+}
+template <typename T> void view(const std::vector<std::vector<T>> &vv) {
+  for(const auto &v : vv) {
+    view(v);
+  }
+}
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+
+  ll count = 0;
+  rep3(x, 1, N + 1) {
+    rep3(y, 1, N + 1) {
+      // z <= M - x - y
+      // z <= N
+      int max_z = min(N, M - x - y);
+      if(max_z <= 0)
+        continue;
+
+      count += max_z;
+    }
+  }
+  debug(count);
+  return 0;
+}
