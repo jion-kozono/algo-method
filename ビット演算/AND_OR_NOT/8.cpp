509156c5051eb2eae123a1caadc32b92e412b88f
jion-kozono <zeon66190329@gmail.com>
2022-09-25 15:28:04 +0900

    AND_OR_NOT

---
+++ ビット演算/AND_OR_NOT/8.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rep3(i, m, n) for(int i = (m); i < (int)(n); i++)
+#define ALL(x) x.begin(), x.end()
+#define debug(var)                                                             \
+  do {                                                                         \
+    view(var);                                                                 \
+  } while(0)
+#define debugN(var)                                                            \
+  do {                                                                         \
+    std::cout << #var << " : ";                                                \
+    view(var);                                                                 \
+  } while(0)
+template <typename T> void view(T e) { std::cout << e << std::endl; }
+template <typename T> void view(const std::vector<T> &v) {
+  for(const auto &e : v) {
+    std::cout << e << " ";
+  }
+  std::cout << std::endl;
+}
+template <typename T> void view(const std::vector<std::vector<T>> &vv) {
+  for(const auto &v : vv) {
+    view(v);
+  }
+}
+
+int main() {
+  ll N, S;
+  cin >> N >> S;
+
+  int Q;
+  cin >> Q;
+  rep(i, Q) {
+    int query, x;
+    cin >> query >> x;
+    if(query == 0) {
+      // On
+      // 論理和
+      S |= (1 << x);
+    } else if(query == 1) {
+      // Off
+      // 論理否定との論理積
+      S &= ~(1 << x);
+    } else {
+      // Check
+      // 論理積
+      if(S & (1 << x))
+        debug("on");
+      else
+        debug("off");
+      // // xビット右にシフトして1が余るか(今回は非効率)
+      // if((S >> x) % 2 == 1)
+      //   debug("on");
+      // else
+      //   debug("off");
+    }
+  }
+  return 0;
+}
