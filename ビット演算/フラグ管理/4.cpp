0aa8efbba1db17fcbf5ecf3d3e19cf67d3dc2713
jion-kozono <zeon66190329@gmail.com>
2022-12-10 16:11:10 +0900

    フラグ管理

---
+++ ビット演算/フラグ管理/4.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rep3(i, m, n) for(int i = (m); i < (int)(n); i++)
+#define ALL(x) x.begin(), x.end()
+#define debug(var)                                                             \
+  do {                                                                         \
+    view(var);                                                                 \
+  } while(0)
+#define debugN(var)                                                            \
+  do {                                                                         \
+    std::cout << #var << " : ";                                                \
+    view(var);                                                                 \
+  } while(0)
+template <typename T> void view(T e) { std::cout << e << std::endl; }
+template <typename T> void view(const std::vector<T> &v) {
+  for(const auto &e : v) {
+    std::cout << e << " ";
+  }
+  std::cout << std::endl;
+}
+template <typename T> void view(const std::vector<std::vector<T>> &vv) {
+  for(const auto &v : vv) {
+    view(v);
+  }
+}
+
+int main() {
+  int N;
+  cin >> N;
+  int F[N];
+  rep(i, N) { cin >> F[i]; }
+
+  ll result = 0;
+  rep(i, N) { result += 1 << F[i]; }
+
+  debug(result);
+  return 0;
+}
